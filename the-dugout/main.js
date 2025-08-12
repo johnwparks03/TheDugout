e89ed8a2421c2b7b06c881bf4f7d593b11af6558
John Parks <johnwparks03@gmail.com>
2025-08-11 20:57:57 -0400

    Upgrade angular version to 16 and add electron to convert to desktop app

---
+++ the-dugout/main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  console.log(
+    "Loading file:",
+    path.join(__dirname, "dist/the-dugout/index.html")
+  );
+  win.loadFile(path.join(__dirname, "dist/the-dugout/index.html"));
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
